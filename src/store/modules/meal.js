9bb94b67d3fd9ccc8405c26f7a6f4c38e89b42ba
Omar20Ashraf <omaarashraf2020@gmail.com>
2023-07-22 16:10:14 +0300

    make search by meal name page

--- src/store/modules/meal.js
+++ src/store/modules/meal.js
@@ -1,16 +1,27 @@
+import axiosClient from "../../axiosClient";
+
 const state = {
+    searchedMeals: [],
 };
 
 const getters = {
-    meals: (state) => state.meals,
+    searchedMeals: (state) => state.searchedMeals,
 };
 
 const actions = {
-
+    async searchMeals({ commit }, keyword) {
+        const response =  await axiosClient.get(`search.php?s=${keyword}`);
+        commit("setSearchedMeals", response.data.meals);
+    },
 };
 
 const mutations = {
+    setSearchedMeals(state,meals){
+        if(!meals)
+            meals = [];
 
+        state.searchedMeals = meals;
+    }
 };
 
 export default {
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,7 +1,68 @@
 <template>
-    <div>
-        Meals By Name
+    <div class="p-8 pb-0">
+        <input 
+            type="text"
+            v-model="keyword"
+            cLass="rounded border-2 border-gray-200 w-full" 
+            placeholder="Search for Meals"
+            @change="searchMeals"
+        />
     </div>
+
+    <div v-if="meals.length" class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+        <div v-for="meal of meals" :key="meal.idMeal" cLass="bg-white shadow rounded-xl">
+
+            <router-link to="/">
+                <img
+                    :src="meal.strMealThumb"
+                    :alt="meal.strMeal"
+                    class="rounded-t-xl w-full h-48 object-cover"
+                />
+            </router-link>
+
+            <div class="p-3">
+                <h3 cLass="font-bold">{{ meal.strMeal }}</h3>
+                <p class="mb-4">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora perspiciatis necessitatibus sint tempore accusamus in saepe fugit hic aperiam!
+                </p>
+                <div class="flex items-center justify-between">
+                    <a 
+                        :href="meal.strYoutube" 
+                        target="_blank" 
+                        class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
+                    >
+                        Youtube
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div v-if="!meals.length" class="flex justify-center text-gray-600 p-8">
+        There are no meals
+    </div>    
 </template>
-<script>
+<script setup>
+
+    import { useStore } from 'vuex';
+    import { ref, computed, onMounted } from "vue";
+    import { useRoute } from 'vue-router';
+
+    const store = useStore();
+    const route = useRoute();
+
+    const keyword = ref('');
+    const meals = computed(() => store.getters.searchedMeals);
+    
+    function searchMeals() {
+        store.dispatch('searchMeals', keyword.value)
+    }
+
+    onMounted(() => {
+        //this incase the user access the url like website/by-name/mealName
+        keyword.value = route.params.name;
+        if(keyword.value){
+            searchMeals()
+        }
+    })
 </script>
